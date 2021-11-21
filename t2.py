5b8fcb5cad1477760a9dda927599e9a2ea9d6ac8
mrslA1k <mrslaiK@rambler.ru>
2021-11-21 16:41:54 +0300

    Update t2.py

--- t2.py
+++ t2.py
@@ -1,20 +1,12 @@
 #вывести целую часть и остаток от деления выражения ((a^2+b^2)/(3b-4))/((4c^5)/6)
-import math
-#s1:str = str(input("Введите число a: "))
-#s2:str = str(input("Введите число b: "))
-#s3:str = str(input("Введите число c: "))
 a:float = float(input("Введите число a: "))
 b:float = float (input("Введите число b: "))
 c:float = float (input("Введите число c: "))
 
-#print(type(s1))
-#res:float=0.0
-#a=s1
-#b=s2
-#c=s3
-res:float = (((a**2+b**2)/(3*b-4))/((4*c**5)/6))
-print("Вариант 1")
-print(f'целая часть {int(res)}, дробная часть {res%1}.')
+res1:float = ((a**2+b**2)/(3*b-4))
+res2:float = ((4*c**5)/6)
+
+print(f'целая часть от деления {res1} на {res2} -  {res1//res2}, дробная часть {res1%res2}.')
 
 print("Bye")
 input("жмякни Интер для выхода")
